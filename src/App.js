31add000799a16b80c4a8711337bec119485b4cb
Franck Monge <franckyroquai@gmail.com>
2023-03-01 17:43:27 +0100

    correction synthaxe

--- src/App.js
+++ src/App.js
@@ -12,24 +12,25 @@ function App() {
           main
         </div>
       </div>
+      <div class="zozo"></div>
 
       Francky c'est cool ;)
-      {/* react, material ui, react-html2pdf */}
-      {/* variables css */}
+      /* react, material ui, react-html2pdf */
+      /* variables css */
 
-      {/* colonne de gauche */}
-      {/* détails personnels - User */}
-      {/* compétences - skills */}
-      {/* langues - skills */}
-      {/* centres d'intérets - interest */}
+      /* colonne de gauche */
+      /* détails personnels - User */
+      /* compétences - skills */
+      /* langues - skills */
+      /* centres d'intérets - interest */
 
-      {/* colonne de droite */}
-      {/* profil - profil */}
-      {/* formations - formations */}
-      {/* exp - eexperiences */}
+      /* colonne de droite */
+      /* profil - profil */
+      /* formations - formations */
+      /* exp - eexperiences */
 
-      {/* light/dark mode */}
-      {/* generation pdf */}
+      /* light/dark mode */
+      /* generation pdf */
     </div>
   );
 }
