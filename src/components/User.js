53e9ab91d4ec708dda75fdcc0b95e1fac910c009
Franck Monge <franckyroquai@gmail.com>
2023-03-02 19:36:07 +0100

    ajout dossier src/component

---
+++ src/components/User.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./User.css"
+import HouseIcon from '@mui/icons-material/House';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import EmailIcon from '@mui/icons-material/Email';
+
+  function User() {
+    return (
+      <div className="user">
+        <img src="./Images/PhotoId.png" alt="" className="user__avatar"/>
+        <h1 className="user__name">Franck Monge</h1>
+        <p className="user__profession">Développeur Web</p>
+        <div className="user__infos">
+            <p className="user__info"><HouseIcon />300 Avenue Amiral Collet 83000 Toulon</p>
+            <p className="user__info"><SmartphoneIcon /><a href="tel:+33612647582">06 12 64 75 82</a></p>
+            <p className="user__info"><EmailIcon /><a href="mailto:franckyroquai@gmail.com">Franckyroquai@gmail.com</a></p>
+            
+        </div>
+      </div>
+    )
+  }
+
+export default User
