0f2ac502a89ff1dbf6ab4fa959a9b1277b0bb09b
Franck Monge <franckyroquai@gmail.com>
2023-03-04 20:26:46 +0100

    création de la fonction Skill (title, rating)

---
+++ src/components/Skill.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Skill({ title, rating }) {
+  return (
+    <div className="skill">
+        <p className="skill__title">{title}</p>
+        <div className="skill__rating">
+            <div className={`circle ${rating > 0 &&"circle-plain"}`}></div>
+            <div className={`circle ${rating > 1 &&"circle-plain"}`}></div>
+            <div className={`circle ${rating > 2 &&"circle-plain"}`}></div>
+            <div className={`circle ${rating > 3 &&"circle-plain"}`}></div>
+            <div className={`circle ${rating > 4 &&"circle-plain"}`}></div>            
+        </div>      
+    </div>
+  )
+}
+
+export default Skill
