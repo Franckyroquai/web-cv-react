713f59795081da6a0eca0a77308ad08b0a60f519
Franck Monge <franckyroquai@gmail.com>
2023-03-04 20:25:31 +0100

    création de la fonction Assets

---
+++ src/components/Assets.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Assets() {
+  return (
+    <div className="skills">
+        <h2 className="h2">Atouts</h2>
+        <ul>
+            <li>Rigoureux</li>
+            <li>Polyvalent</li>
+            <li>Curieux</li>
+            <li>Esprit d'équipe</li>
+            <li>Autonome</li>
+            <li>Organisé</li>
+            <li>Capacité d'analyse</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Assets
