175a9c1e4fbdec42eea60e5589f9ed18fadce5c1
Franck Monge <franckyroquai@gmail.com>
2023-03-07 17:22:07 +0100

    création de la fonction profil

---
+++ src/components/Profil.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Profil() {
+  return (
+    <div className="profil">
+      <h2 className="h2">Développeur Web Full-Stack Junior</h2>
+      <p>
+        "Un expert est quelqu’un qui a fait toutes les erreurs de faire dans un domaine".
+        <br/><i>Niels Bohr</i>
+      </p>
+    </div>
+  )
+}
+
+export default Profil
