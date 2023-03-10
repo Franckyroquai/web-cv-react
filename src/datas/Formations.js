61ab7ad16a7cc2524f6078ce6db075653b2babf1
Franck Monge <franckyroquai@gmail.com>
2023-03-10 19:19:00 +0100

    création fichier datas pour les formations et l'expérience

---
+++ src/datas/Formations.js
@@ -0,0 +1,40 @@
+const dataFormations = [
+    {
+        id:1,
+        title:"Titre Professionnel de développeur Web et Web-Mobile",
+        date:"2021 - 2022",
+        location:"Afpa - La Valette du Var"
+    },
+    {
+        id:2,
+        title:"Attestation Arduino : Programmation d’objet",
+        date:"2021",
+        location:"Université en ligne FUN MOOC"
+    },
+    {
+        id:3,
+        title:"Baccalauréat Professionnel Electronique",
+        date:"2017 - 2019",
+        location:"IPFM - La Seyne sur Mer"
+    },
+    {
+        id:4,
+        title:"Diploma of Higher Education in Business and Technology ",
+        date:"1998 - 1999",
+        location:"Université de Coventry (Angleterre)"
+    },
+    {
+        id:5,
+        title:"DUT Techniques de commercialisation ",
+        date:"1996 - 1998",
+        location:"Université de Toulon - La Garde"
+    },
+    {
+        id:6,
+        title:"Baccalauréat Economique et Social",
+        date:"1995",
+        location:"Lycée Maintenon - Hyères"
+    },
+]
+
+export default dataFormations
