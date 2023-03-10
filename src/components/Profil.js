f8601824b1628163bcdd0f4fc49d427b553aaa3f
Franck Monge <franckyroquai@gmail.com>
2023-03-10 19:13:34 +0100

    ajout marging bottom

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Profil() {
   return (
-    <div className="profil">
+    <div className="profil mb5">
       <h2 className="h2">Développeur Web Full-Stack Junior</h2>
       <p>
         "Un expert est quelqu’un qui a fait toutes les erreurs de faire dans un domaine".
