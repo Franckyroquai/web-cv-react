8700e8fb0de1025f5c460ae42fc833b64a3bccfd
Franck Monge <franckyroquai@gmail.com>
2023-03-11 20:57:04 +0100

    création du tableau des experiences

---
+++ src/datas/Experiences.js
@@ -0,0 +1,143 @@
+const dataExperiences = [
+    {
+        id:1,
+        title:"Développeur Web et Web-Mobile",
+        date:"2021 - 2022",
+        location:"AFPA - La Valette du Var",
+        text:`Formation au métier de développeur web et web-mobile`,
+        missions: [
+            {
+                id:1,
+                title: "Appréhender les langages HTML, CSS, JavaScript et MySQL.",
+            },
+            {
+                id:2,
+                title: "Utilisation des outils Vue.js, Node.js, Express.js, Sequelize, Figma, Agile et GitHub.",
+            },
+            {
+                id:3,
+                title: "Création d'un site internet.",
+            },
+        ],
+    },
+    {
+        id:2,
+        title:"Technicien nautique",
+        date:"2018",
+        location:"KIRIACOULIS POINT D’AMURE - Bormes les Mimosas",
+        text:`Stage de 7 semaines dans le cadre d'un baccalauréat électronique`,
+        missions: [
+            {
+                id:1,
+                title: "Entretien général de voiliers de location et préparation de voiliers neufs.",
+            },
+            {
+                id:2,
+                title: "Notions pratiques en électronique, mécanique, voilerie et stratification.",
+            },
+        ],
+    },
+    {
+        id:3,
+        title:"Gestionnaire sinistres / Conseiller en assurances",
+        date:"2000 - 2019",
+        location:"MATMUT - Toulon",
+        text:`CDI`,
+        missions: [
+            {
+                id:1,
+                title: "Traitement complet des dossiers sinistres dans le cadre adapté (droit commun, conventions, garanties du contrat).",
+            },
+            {
+                id:2,
+                title: "Proposer des solutions assurancielles adaptées à la situation de chacun en exerçant le rôle de devoir de conseil.",
+            },
+            {
+                id:3,
+                title: "Gestion de la relation client en vis à vis et par téléphone.",
+            },
+        ],
+    },
+    {
+        id:4,
+        title:"Guichetier",
+        date:"2000",
+        location:"CREDIT AGRICOLE - Cavalaire sur Mer",
+        text:`CDD`,
+        missions: [
+            {
+                id:1,
+                title: "Accueil et orientation clientèle.",
+            },
+            {
+                id:2,
+                title:"Opérations bancaires courantes (dépôts, retraits, virements, prise de rendez-vous avec un conseiller).",
+            },
+            {
+                id:3,
+                title: "Responsabilité d’une caisse, réactivité face aux demandes des clients.",
+            },
+        ],
+    },
+    {
+        id:5,
+        title:"Chargé de portefeuille locations saisonnières",
+        date:"1997 - 1999",
+        location:"AGENCE IMMOBILIERE LA SOLEIADE - Le Lavandou",
+        text:`Saisons estivales`,
+        missions: [
+            {
+                id:1,
+                title: "Accueil et installation des locataires",
+            },
+            {
+                id:2,
+                title:"Rédactions d'inventaires",
+            },
+            {
+                id:3,
+                title: "Trouver les solutions adaptées face aux doléances du quotidien",
+            },
+        ],
+    },
+    {
+        id:6,
+        title:"Etude de lancement commercial d’un téléphone portable innovant",
+        date:"1998 - 1999",
+        location:"UNIVERSITE DE COVENTRY (Angleterre)",
+        text:`Projet d'étude`,
+        missions: [
+            {
+                id:1,
+                title: "Analyse technique des différentes étapes de lancement d'un produit technologique innovant (conception, étude marketing, production, communication et diagnostic financier).",
+            },
+            {
+                id:2,
+                title:"Capacité d’intégration et de travail dans un groupe d'étudiants européens.",
+            },
+            {
+                id:3,
+                title: "S'exprimer en anglais à l'oral et à l'écrit.",
+            },
+        ],
+    },
+    {
+        id:7,
+        title:"Plagiste",
+        date:"1993 - 1996",
+        location:"TAHITI PLAGE - St Clair",
+        text:`Saisons estivales`,
+        missions: [
+            {
+                id:1,
+                title: "Installation et rangement du matériel de plage",
+            },
+            {
+                id:2,
+                title:"Service en restauration (dressage de table, accueil client, prise de commandes, servir et débarrasser).",
+            },
+        ],
+    },
+]
+
+export default dataExperiences
