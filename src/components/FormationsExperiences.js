532d8b03be42c68b76e2ecb959d90b8d818673bf
Franck Monge <franckyroquai@gmail.com>
2023-03-11 20:56:16 +0100

    appel de la fonction experiences

--- src/components/FormationsExperiences.js
+++ src/components/FormationsExperiences.js
@@ -2,11 +2,14 @@ import React from 'react'
 import "./FormationsExperiences.css"
 import dataFormations from "../datas/Formations"
 import Formations from "./Formations"
+import dataExperiences from "../datas/Experiences"
+import Experiences from "./Experiences"
 
 function FormationsExperiences() {
   return (
     <>
       <Formations datas={dataFormations} />
+      <Experiences datas={dataExperiences} />
     </>
   )
 }
