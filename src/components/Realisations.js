61cffa9f8195685d65d6f14478aee6ef21533bd3
Franck Monge <franckyroquai@gmail.com>
2023-03-18 19:00:58 +0100

    creation du'une partie réalisations

---
+++ src/components/Realisations.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Realisations() {
+  return (
+    <div className="realisations mb5">
+      <h2 className="h2">Réalisation</h2>
+      <section className="VValentin">
+          <h3>
+            Création d'un site internet pour une psychologue professionnelle
+          </h3>
+          <h3>
+              <i><a href="https://vvalentin.fr/" target="blank">https://vvalentin.fr</a></i>
+          </h3>
+          <iframe title="FrameVValentin"
+                id="FrameVValentin"
+                width="60%"
+                height="370px"
+                src="https://vvalentin.fr/">
+                </iframe>
+      </section>
+      
+    </div>
+  )
+}
+
+export default Realisations
