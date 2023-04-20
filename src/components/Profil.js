7ead64412c40d3afebb0155ad59910affc0443fc
Franck Monge <franckyroquai@gmail.com>
2023-04-20 20:49:11 +0200

    ajout description et changement citation

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -4,9 +4,10 @@ function Profil() {
   return (
     <div className="profil mb5">
       <h2 className="h2">Développeur Web Full-Stack Junior</h2>
-      <p>
-        "Un expert est quelqu’un qui a fait toutes les erreurs de faire dans un domaine".
-        <br/><i>Niels Bohr</i>
+      <p className="presentation">
+      En reconversion professionnelle, je recherche une entreprise qui pourra m'offrir l'opportunité d'acquérir de l'expérience concrète tout en développant mes compétences techniques.      </p>
+      <p className="citation">
+        <i>Allons ensemble où nos rêves nous portent...</i>
       </p>
     </div>
   )
