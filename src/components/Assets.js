c541650a113e10028bcf2c928de04dbdc717eec6
franckyroquai <franckyroquai@gmail.com>
2024-05-01 15:12:27 +0200

    actualisation Atouts

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -5,13 +5,10 @@ function Assets() {
     <div className="skills">
         <h2 className="h2">Atouts</h2>
         <ul>
-            <li>Rigoureux</li>
-            <li>Polyvalent</li>
-            <li>Curieux</li>
-            <li>Esprit d'équipe</li>
-            <li>Autonome</li>
-            <li>Organisé</li>
-            <li>Capacité d'analyse</li>
+            <li>Organisation, rigueur</li>
+            <li>Autonomie, persévérance</li>
+            <li>Etre à l'écoute, inspirer</li>
+            <li>Curiosité, créativité</li>
         </ul>
       
     </div>
