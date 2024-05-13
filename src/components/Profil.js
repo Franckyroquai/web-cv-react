f9d01be861b2f6d6c29188c0e25f317ef7dc9b5c
Franck Monge <90770156+Franckyroquai@users.noreply.github.com>
2024-05-13 22:31:30 +0200

    Update Profil.js
    Maj présentation

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -5,7 +5,8 @@ function Profil() {
     <div className="profil mb5">
       <h2 className="h2">Développeur Web Full-Stack Junior</h2>
       <p className="presentation">
-      En reconversion professionnelle, je recherche une entreprise qui pourra m'offrir l'opportunité d'acquérir de l'expérience concrète tout en développant mes compétences techniques.      </p>
+      Je cherche en tant que développeur web junior à intégrer une équipe où je peux contribuer au développement d'applications web innovantes, tout en continuant à développer mes compétences techniques.
+      </p>
       <p className="citation">
         <i>Allons ensemble où nos rêves nous portent...</i>
       </p>
