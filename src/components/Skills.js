cadeb774d0bfd1d5d60e21bd2772d9fd50e517c5
franckyroquai <franckyroquai@gmail.com>
2024-09-27 17:30:56 +0200

    Maj Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,17 +9,20 @@ function Skills() {
     <>
         <div className="skills">
             <h2 className="h2">Compétences</h2>
-            <Skill title="GitHub" rating="4" />      
+            <Skill title="Git / GitHub" rating="4" />      
             <Skill title="HTML" rating="5" />      
             <Skill title="CSS" rating="4" />      
+            <Skill title="Algorithmique" rating="3" />
             <Skill title="JavaScript" rating="3" />
+            <Skill title="Méthode Merise" rating="4" />
             <Skill title="Node.JS" rating="3" />      
-            <Skill title="PHP" rating="3" />
             <Skill title="Vue.JS" rating="3" />     
             <Skill title="React" rating="3" />
             <Skill title="Angular" rating="3" />          
+            <Skill title="PHP" rating="3" />
             <Skill title="Symfony" rating="3" />      
             <Skill title="WordPress" rating="4" />      
+            <Skill title="UI / UX" rating="3" />           
             <Skill title="SEO" rating="3" />           
         </div>
         <div className="skills">
